16a7152e9b5c769c14749e479c838bba70c870d2
Cearjeyou <harturista@gmail.com>
2021-07-17 15:30:13 -0500

    first commit

---
+++ src/Model/Tripulante.java
@@ -0,0 +1,68 @@
+package Model;
+
+import javax.lang.model.util.ElementScanner6;
+
+public class Tripulante {
+    /**
+     * Atributos
+     */
+    private String nombre;
+    private int Id;
+    private double nota1;
+    private double nota2;
+    private double nota3;
+    /**Getters */
+    public String getNombre() {
+        return nombre;
+    }
+    public int getId() {
+        return Id;
+    }
+    public double getNota1() {
+        return nota1;
+    }
+    public double getNota2() {
+        return nota2;
+    }
+    public double getNota3() {
+        return nota3;
+    }
+    /**Setters */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setId(int id) {
+        Id = id;
+    }
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+    public double getAverage() {
+        return(nota1 + nota2 + nota3)/3;        
+    }
+    public double getHighNote() {
+            if (nota1 >= nota2 && nota1 >= nota2) {
+                return nota1;
+        } else if (nota2 >= nota3) {
+            return nota2;            
+        } else {
+            return nota3;
+        }
+    }
+    public double getLowNote() {
+        if (nota1 <= nota2 && nota1 <= nota3) {
+            return nota1;           
+        } else if (nota2 <= nota3) {
+            return nota2;
+        } else {
+            return nota3;
+        }
+    }
+    
+}
